c7d6d0aa6aa99a255755bf1b34a4b98fd8ce6b57
Chandler McWilliams <chandler@brysonian.com>
2017-11-21 15:29:49 -0800

    Add speaking demo

--- src/sketch.js
+++ src/sketch.js
@@ -1,52 +1,16 @@
-let vitos = [];
+let speaker;
 
 function setup() {
   createCanvas(800, 800);
-  noStroke();
-  fill(0, 20);
 
-  for (let i=0; i<200; i++) {
-    vitos.push(new Follower());
-  }
+  speaker = new p5.Speech();
+  speaker.speak("hello world");
 }
 
 function draw() {
-  background(255);
 
-  for (let i=0; i<vitos.length; i++) {
-  	vitos[i].render();
-  	vitos[i].update();
-  }
 }
 
 function mousePressed() {
-  for (let i=0; i<vitos.length; i++) {
-	  vitos[i].setTarget(random(width), random(height));
-  }
-}
-
-
-class Follower {
-  constructor() {
-    this.targetX = random(width);
-    this.targetY = random(height);
-    this.x = 0;
-    this.y = 0;
-    this.coef = random(0.05, 0.25);
-  }
-
-  setTarget(newTargetX, newTargetY) {
-    this.targetX = newTargetX;
-    this.targetY = newTargetY;
-  }
-
-  render() {
-    ellipse(this.x, this.y, 100, 100);
-    ellipse(x,y,w,[h])
-  }
-
-  update() {
-	  this.x += (this.targetX - this.x) * this.coef;
-	  this.y += (this.targetY - this.y) * this.coef;
-  }
+  speaker.speak("Lorem ipsum");
 }
