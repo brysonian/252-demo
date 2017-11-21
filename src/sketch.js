88c43737dbf91fb923c93e8b040a81d3a7883855
Chandler McWilliams <chandler@brysonian.com>
2017-11-21 14:42:46 -0800

    Remove some whitespace

--- src/sketch.js
+++ src/sketch.js
@@ -1,39 +1,29 @@
 let vitos = [];
 
-function setup() { 
+function setup() {
   createCanvas(800, 800);
   noStroke();
   fill(0, 20);
-  
+
   for (let i=0; i<200; i++) {
     vitos.push(new Follower());
   }
-} 
+}
 
-function draw() { 
+function draw() {
   background(255);
-  
-  for (let i=0; i<vitos.length; i++) {  
+
+  for (let i=0; i<vitos.length; i++) {
   	vitos[i].render();
   	vitos[i].update();
   }
 }
 
 function mousePressed() {
-  for (let i=0; i<vitos.length; i++) {  
+  for (let i=0; i<vitos.length; i++) {
 	  vitos[i].setTarget(random(width), random(height));
   }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 class Follower {
@@ -44,20 +34,19 @@ class Follower {
     this.y = 0;
     this.coef = random(0.05, 0.25);
   }
-  
+
   setTarget(newTargetX, newTargetY) {
     this.targetX = newTargetX;
     this.targetY = newTargetY;
   }
-  
+
   render() {
     ellipse(this.x, this.y, 100, 100);
+    ellipse(x,y,w,[h])
   }
-  
+
   update() {
 	  this.x += (this.targetX - this.x) * this.coef;
-	  this.y += (this.targetY - this.y) * this.coef;    
+	  this.y += (this.targetY - this.y) * this.coef;
   }
 }
-
-
